2d27e8a6dea1b894f25a67a1b45633eade61c73b
MarcusJones <jones.0bj3@gmail.com>
2021-07-12 22:42:03 +0200

    update pipe random hash into curl

--- image/entrypoint.sh
+++ image/entrypoint.sh
@@ -4,6 +4,17 @@ echo $# Args: $@
 echo "Environment:"
 env
 
-# curl -d '{"key1":"value1", "key2":"value2"}' -H "Content-Type: application/json" -X POST http://localhost:$PORT
-echo curl -d '{"key1":"value1", "key2":"value2"}' -H "Content-Type: application/json" -X POST http://$ENDPOINT:$PORT
-curl -d '{"key1":"value1", "key2":"value2"}' -H "Content-Type: application/json" -X POST http://$ENDPOINT:$PORT
+# echo head -n 4096 /dev/urandom | sha256sum
+# mystring=$(head -n 4096 /dev/urandom | sha256sum) | cut -f 1 -d " "
+
+# Get 4096 bytes of random data. Take the 256 hash. Do not keep the dash after the string. Assign to variable.
+mystring=$(head -n 4096 /dev/urandom | sha256sum | cut -f 1 -d " ")
+echo $mystring
+
+# Build a JSON string. The JSON data may have spaces, newlines, etc.
+JSON_STRING=$( jq -n -r --arg hs "$mystring" '{"random hash": $hs}' )
+echo $JSON_STRING
+
+# Can't simply use -d $JSON_STRING, as this has newlines, spaces.
+# Instead, pipe it into the command.
+echo $JSON_STRING | curl -d @- -H "Content-Type: application/json" -X POST http://$ENDPOINT:$PORT
