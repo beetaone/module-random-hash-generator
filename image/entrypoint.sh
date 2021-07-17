a3a40b05ffe0646d9ac50311671d4dce1cfdf8ab
MarcusJones <jones.0bj3@gmail.com>
2021-07-17 10:34:06 +0200

    update entrypoint validation

--- image/entrypoint.sh
+++ image/entrypoint.sh
@@ -1,22 +1,59 @@
 #!/bin/sh
 echo "Entrypoint script"
-echo $# Args: $@
-echo "Environment:"
-env
 
-if test [ -f /mounted ] ; then
-    echo "Mounted"
-else
-    exit Error: Expected a mounted file at /mounted
+# Assert number of args
+if [ ! $# -eq 1 ]
+  then
+    echo "Expected 1 argument, either [sha256, sha1, or md5]"
+    exit 1
 fi
 
-# echo head -n 4096 /dev/urandom | sha256sum
-# mystring=$(head -n 4096 /dev/urandom | sha256sum) | cut -f 1 -d " "
 
-# Get 4096 bytes of random data. Take the 256 hash. Do not keep the dash after the string. Assign to variable.
-randomstring=$(head -n 4096 /mounted | sha256sum | cut -f 1 -d " ")
-# echo $mystring
-echo "Generated random hash $randomstring from host"
+# Check volume mounts
+# if [ ! -f /mounted ]
+# then
+#     echo "Entrypoint validation error: Expected a file-like object at /mounted"
+#     exit 1
+# fi
+
+# Assert hash functions exist as executables
+if ! [ -x "$(command -v md5sum)" ]
+then
+    echo "md5sum could not be found"
+    exit
+fi
+if ! [ -x "$(command -v sha1sum)" ]
+then
+    echo "sha1sum could not be found"
+    exit
+fi
+if ! [ -x "$(command -v sha256sum)" ]
+then
+    echo "sha256sum could not be found"
+    exit
+fi
+
+# Assert parameter matches
+case $1 in
+    md5)
+        # Get 4096 bytes of random data. Take the hash. Do not keep the dash after the string. Assign to variable.
+        randomstring=$(head -n 4096 /mounted | md51sum | cut -f 1 -d " ")
+        echo "Generated random SHA1 hash $randomstring from host"
+    ;;
+    sha1)
+        randomstring=$(head -n 4096 /mounted | sha1sum | cut -f 1 -d " ")
+        echo "Generated random SHA1 hash $randomstring from host"
+    ;;
+    sha256)
+        randomstring=$(head -n 4096 /mounted | sha256sum | cut -f 1 -d " ")
+        echo "Generated random SHA256 hash $randomstring from host"
+    ;;
+    *)
+        echo "Entrypoint validation error: expected [sha256, sha1, or md5]"
+        exit 1
+esac
+
+# randomstring=$(head -n 4096 /mounted | sha256sum | cut -f 1 -d " ")
 
 # Build a JSON string. The JSON data may have spaces, newlines, etc.
 JSON_STRING=$( jq -n -r --arg hs "$randomstring" '{"random hash": $hs}' )
