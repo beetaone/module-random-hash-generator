382e051560f6cd0ae4602cb40dcffa611c598325
MarcusJones <jones.0bj3@gmail.com>
2021-07-25 09:04:17 +0200

    update logging

--- image/main.sh
+++ image/main.sh
@@ -7,15 +7,15 @@ while true; do
     md5)
         # Get 4096 bytes of random data. Take the hash. Do not keep the dash after the string. Assign to variable.
         randomstring=$(head -n 4096 $VOLUME_CONTAINER | md51sum | cut -f 1 -d " ")
-        echo -e "\ndev-random generated random SHA1 hash $randomstring from host"
+        echo -e "\ndev-random: generated random SHA1 hash $randomstring from host"
         ;;
     sha1)
         randomstring=$(head -n 4096 $VOLUME_CONTAINER | sha1sum | cut -f 1 -d " ")
-        echo -e "\ndev-random generated random SHA1 hash $randomstring from host"
+        echo -e "\ndev-random: generated random SHA1 hash $randomstring from host"
         ;;
     sha256)
         randomstring=$(head -n 4096 $VOLUME_CONTAINER | sha256sum | cut -f 1 -d " ")
-        echo -e "\ndev-random generated random SHA256 hash $randomstring from host"
+        echo -e "\ndev-random: generated random SHA256 hash $randomstring from host"
         ;;
     *)
         echo "Validation error: expected --hash=[sha256, sha1, or md5]"
@@ -29,7 +29,7 @@ while true; do
 
     # Can't simply use -d $JSON_STRING, as this has newlines, spaces.
     # Instead, pipe it into the command.
-    echo "POST to http://$ENDPOINT:$PORT"
+    echo -e "\ndev-random: POST to http://$ENDPOINT:$PORT"
     # POST this to the target
     echo $JSON_STRING | curl -d @- -H "Content-Type: application/json" -X POST http://$ENDPOINT:$PORT || echo "Curl exited with status $?"
 
